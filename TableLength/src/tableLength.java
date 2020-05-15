4b0a5650e14f6c8bfe94ea93bc93f5fad41768c5
PatrycjaGrzybek <64777861+PatrycjaGrzybek@users.noreply.github.com>
2020-05-15 20:06:12 +0200

    First commit

---
+++ TableLength/src/tableLength.java
@@ -0,0 +1,9 @@
+public class tableLength {
+	public static void main(String[] args) {
+		int tab[] = {1,2,3,4,5,6,5,4,3,2,1};
+		int i =0;
+		while (i <tab.length) {
+			System.out.println("tab["+i+"] ="+ tab[i++]);
+		}
+	}
+}
